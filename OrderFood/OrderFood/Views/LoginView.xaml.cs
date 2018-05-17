21159b2b566ea61fbbb07278fc6356d3ceab4575
Alex Bobel <alex.bobel@headworks.com.ua>
2018-05-17 20:31:33 +0300

    Created project

---
+++ OrderFood/OrderFood/Views/LoginView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace OrderFood.Views
+{
+    public partial class LoginView : ContentPage
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+    }
+}
